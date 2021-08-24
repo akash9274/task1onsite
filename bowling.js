5a4bd5447fd75c6c2455f82f9286c075244d6f45
akash9274 <83513701+akash9274@users.noreply.github.com>
2021-08-24 17:26:52 +0400

    Add files via upload

---
+++ bowling.js
@@ -0,0 +1,31 @@
+var randomnum
+
+var score = 0
+var newnumb = document.createElement("div")
+
+
+function getvalue(x){
+    console.log(x)
+
+    var randomnum=Math.floor(Math.random()*6)+1
+    const playernumb =document.createElement("div")
+    const newcontent= document.createTextNode("PLAYER NUMBER : " + x)
+
+    playernumb.appendChild(newcontent)
+    document.body.appendChild(playernumb)
+
+    const computernumb =document.createElement("div")
+    const newcontent2= document.createTextNode("COMPUTER GENERATED NUMBER : " + randomnum)
+
+    computernumb.appendChild(newcontent2)
+    document.body.appendChild(computernumb)
+
+    if(randomnum==x){
+        alert("YOU HAVE BEEN BOWLED OUT WITH SCORE OF "+score)
+        document.location.reload()
+    }
+    else
+    score= score+randomnum
+
+    
+}
